261ce65afb4f8f5b75c245a072446187af772c6f
Yedam <waterclean101@gmail.com>
2022-11-09 14:35:59 +0900

    first commit

---
+++ finalProject/article/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Article(models.Model):
+
+    title = models.TextField()  # 기사제목
+    reporter = models.CharField(max_length=50)  # 기자
+    press = models.CharField(max_length=50)  # 언론사
+    link = models.URLField()  # 기사링크
+    publication_time = models.DateTimeField()  # 발행시간
+    publication_str = models.TextField()  # 발행시간 문자열
+    result = models.TextField()  # 낚시성인지 결과
+    crawling_time = models.DateTimeField()  # 크롤링한 시간
+
+    def __str__(self):
+        return self.title
